01f450483accd976486ae91c5602e944db7b18bc
lcy2080 <lcy2080@gmail.com>
2019-05-28 00:11:04 +0900

    step2, step3 - build with other project, and install

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include "Tuto0Config.h"
 
-//#ifdef USE_MYMATH
-//#include "MathFunctions.h"
-//#else
-//#include <math.h>
-//#endif
+#ifdef USE_MYMATH
+#include "MathFunctions.h"
+#else
+#include <math.h>
+#endif
 
 
 int main()
 {
     std::cout << "hello world " << TUTO0_VERSION_MAJOR << "." << TUTO0_VERSION_MINOR << std::endl;
-//    std::cout << "SQRT Out : " << sqrt(3) << std::endl;
+
+#ifdef USE_MYMATH
+    std::cout << "SQRT Out : " << mysqrt(3) << std::endl;
+#else
+    std::cout << "SQRT Out : " << sqrt(3) << std::endl;
+#endif
     return 0;
 }
